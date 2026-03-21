497bfd112d47ccdcb4154e8ce1cad681447e7ca1
Nicky <phamquocbao2401@gmail.com>
2026-03-22 05:14:48 +0700

    block_number.sh

---
+++ scripts/block_number.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RPC_URL="https://rpc.ankr.com/eth"
+
+curl -s -X POST $RPC_URL \
+-H "Content-Type: application/json" \
+--data '{
+  "jsonrpc":"2.0",
+  "method":"eth_blockNumber",
+  "params":[],
+  "id":1
+}' | jq '.result'
